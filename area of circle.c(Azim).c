f661638891b8d7f15022075d5d80cdbc18d7ddfe
Azim sherasiya <115138533+AZIMSHERASIYA@users.noreply.github.com>
2022-10-17 21:29:33 +0530

    Add files via upload

---
+++ area of circle.c(Azim).c
@@ -0,0 +1,22 @@
+//WAP AREA OF PERIMETER
+#include<stdio.h>
+#define PI 3.14
+int main()
+{
+	float area,radius,perimeter;
+	printf("enter the radius of a circle");
+	scanf("%f",&radius);
+	area=PI*radius*radius;
+	perimeter=2*PI*radius;
+	printf("the area of circle=%f\n perimeter of circle=%f ",area,perimeter);
+	return 0;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
