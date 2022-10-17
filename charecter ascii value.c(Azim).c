c7c2f4fcc6a24d4f4edcf99d6de69b523e1d0653
Azim sherasiya <115138533+AZIMSHERASIYA@users.noreply.github.com>
2022-10-17 21:49:31 +0530

    Add files via upload

---
+++ charecter ascii value.c(Azim).c
@@ -0,0 +1,10 @@
+//WAP read your charecter to ASCII value
+#include<stdio.h>
+int main()
+{
+	char a;
+	printf("enter any chareter");
+	scanf("%c",&a);
+	printf("the ascii value of charecter is=%d,a");
+	return 0;
+}
