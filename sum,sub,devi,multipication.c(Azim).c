2a9b03102dcba89cca7b270f931cf5280cd89a6a
Azim sherasiya <115138533+AZIMSHERASIYA@users.noreply.github.com>
2022-10-17 21:40:40 +0530

    Add files via upload

---
+++ sum,sub,devi,multipication.c(Azim).c
@@ -0,0 +1,17 @@
+//WAP sum,sub,multipication,devi
+#include<stdio.h>
+int main ()
+{
+	int a,b,sum,sub,multi,devi;
+	printf("enter any two number");
+	scanf("%d %d",&a,&b);
+	sum=a+b;
+	printf("value of sum=%d\n",sum);
+	sub=a-b;
+	printf("values of sub=%d\n",sub);
+	multi=a*b;
+	printf("values of multi=%d\n",multi);
+	devi=a/b;
+	printf("values of number=%d\n",devi);
+	return 0;
+}
